f4ae11d5b6a125e24dcc269327bc2c6c6698d2fe
libepe <liam.beus@gmail.com>
2025-05-06 22:08:43 +0200

    mod nueva

--- js/web.js
+++ js/web.js
@@ -13,6 +13,7 @@ function showHeader(){
     .then(data => {
         header.innerHTML=data;
         colorHF();
+        clickShow();
     })
 }
 
@@ -31,9 +32,37 @@ function showFooter(){
 
 //HOME/
 
+function clickShow(){
+    const fondo=document.getElementById('fondo');
+
+    fondo.addEventListener('click', () => {
+        const header=document.querySelector('header'); 
+        if (header) {
+            header.classList.toggle('header-activo'); 
+        }
+    });
+    
+}
+
+
+
 //COLORES FONDO//
+
+function colorOscuro(color, oscuro=0.8) {
+    let r=parseInt(color.slice(1, 3), 16);
+    let g=parseInt(color.slice(3, 5), 16);
+    let b=parseInt(color.slice(5, 7), 16);
+
+    r=Math.floor(r*oscuro);
+    g=Math.floor(g*oscuro);
+    b=Math.floor(b*oscuro);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function colorMain(){
     const bCs=document.querySelectorAll('.bc');
+    const bRs=document.querySelectorAll('.br');
     const Cs=document.querySelectorAll('.c');
     const colors=document.querySelectorAll('.color');
     colors.forEach((color) => {
@@ -43,28 +72,36 @@ function colorMain(){
         function cambioColor(value){
             bCs.forEach((bc => bc.style.backgroundColor=value));
             Cs.forEach((c => c.style.color=value));
+            bRs.forEach((br => br.style.borderColor=colorOscuro(value, 0.8)));
         }
 
         color.addEventListener('click',function(){
             cambioColor(value);
             colors.forEach(c => c.classList.remove('seleccionado'));
             color.classList.add('seleccionado');
+
+            document.querySelectorAll('.bc').forEach(bc => {
+                bc.style.backgroundColor=value;
+            });
+
         })
 
-        if(value=='#CCCC00') {
+        if(value=='#4d1b1b') {
             cambioColor(value);
             color.classList.add('seleccionado');
         }
     });
+
 }
 colorMain();
+
+
 
 
 function colorHF(){
     const Hs=document.querySelectorAll('.h');
     const colors=document.querySelectorAll('.color');
-    console.log('Found .h elements:', Hs);
-    let value='#CCCC00';
+    let value='#4d1b1b';
 
     colors.forEach((color) => {
         color.addEventListener('click',()=>{
@@ -75,12 +112,12 @@ function colorHF(){
     Hs.forEach((h) => {
         h.addEventListener('mouseover',()=>{
             if(value){
-                h.style.color=value;
+                h.style.backgroundColor=value;
             } 
         });
 
         h.addEventListener('mouseout',()=>{
-            h.style.color='';
+            h.style.backgroundColor='';
         });
     })
 }
