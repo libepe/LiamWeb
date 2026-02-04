07a78782d5eb9ba01e8c5d3dd596add78355f191
libepe <liam.beus@gmail.com>
2026-02-04 11:02:16 +0100

    mod nueva

--- js/general.js
+++ js/general.js
@@ -1,41 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    toPagInicio();
+    portfolioDeploy();
 });
 
 
 
 
+function portfolioDeploy(){
 
+    const opener=document.getElementById('opener');
+    const startWidth = 0;
 
-
-function toPagInicio(){
-
-
-    const toInicio = document.getElementById('inicio');
-    const paginicio = document.getElementById('pag-inicio');
-    const pagcontacto = document.getElementById('pag-contacto');
-    const pagportfolio=document.getElementById('pag-portfolio');
-
-    toInicio.addEventListener('click', e => {
-        e.preventDefault();
-
-        [ paginicio, pagportfolio, pagcontacto].forEach(p => {
-            p.classList.remove('aparece', 'desaparece', 'pordefecto');
-        });
-
-        pagportfolio.classList.add('desaparece');      
-        pagcontacto.classList.add('desaparece');
-
-        paginicio.classList.add('aparece');
-
-       
-        
-
-              cambiosDisenoPagPortfolio();
-              cambiosDisenoPagContacto();
-
-      })
+     window.addEventListener('scroll', () => {
+        opener.style.width = startWidth + window.scrollY + 'px';
+  });
 }
+
+
+
   
 
