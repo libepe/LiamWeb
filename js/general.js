31e5f0461702b1ca7a9c84af422bcaf3e579d01f
libepe <liam.beus@gmail.com>
2026-02-05 22:37:10 +0100

    mod nueva

--- js/general.js
+++ js/general.js
@@ -1,19 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     portfolioDeploy();
+    centeredPage();
 });
 
 
+function centeredPage(){
+    window.addEventListener('load', () => {
+        const middle = document.documentElement.scrollHeight / 2;
+    window.scrollTo(0, middle);
+    });
+}
+
+function portfolioDeploy() {
+  const opener = document.getElementById('opener');
+  const paginicio=document.getElementById('pag-inicio');
 
 
-function portfolioDeploy(){
+  window.addEventListener('scroll', () => {
+    const scale = 1 + window.scrollY / 20;
+    const opacity= 1 - window.scrollY / 300;
 
-    const pagportfolio=document.getElementById('pagportfolio');
-    const startWidth = 0;
-
-     window.addEventListener('scroll', () => {
-        pagportfolio.style.width = startWidth + window.scrollY + 'px';
+    opener.style.transform = `scale(${scale}) rotateZ(-135deg)`;
+    paginicio.style.opacity= `${opacity}`;
   });
 }
+
+
+
+
 
 
 
