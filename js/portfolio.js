cd1778f9e3115a5162a8f5784bf7caded2625c34
Liam Beus Pérez <liam.beus@gmail.com>
2026-02-11 19:17:53 +0000

    mod nueva

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,23 +13,34 @@ function portfolioDeploy() {
 
   const principio={
     brillo: parseFloat(estiloParent.getPropertyValue('--brillo')),
-    rotacion: 0,
+    ancho: parseFloat(estiloParent.getPropertyValue('--ancho')),
+    alto: parseFloat(estiloParent.getPropertyValue('--alto')),
     fontsize: parseFloat(estiloTitulo.getPropertyValue('--fontsize')),
+    opacidad: parseFloat(estiloTitulo.getPropertyValue('--opacity')),
+    rotacion: parseFloat(estiloParent.getPropertyValue('--rotate')),
   }
 
   const rapidez={
     tamanyo: 2.5,
     opacidad: 0.002,
-    rotacion: 0.1,
-    brillo: 0.05,
-    fontsize: 0.5,
+    rotacion: 0.07,
+    brillo: 0.08,
+    rapidezTamanyo: 0.006,
   }
 
   window.addEventListener('scroll', () => {
 
+    const tamanyo = 1 + window.scrollY * rapidez.rapidezTamanyo;
+
+
+
+    scrollportfolio.style.setProperty('--ancho', principio.ancho * tamanyo + 'px');
+    scrollportfolio.style.setProperty('--alto', principio.alto * tamanyo + 'px');
+    scrollportfolioTitulo.style.setProperty('--fontsize', principio.fontsize * tamanyo + 'px');
+
     scrollportfolio.style.setProperty('--brillo', principio.brillo - window.scrollY * rapidez.brillo + '%');
-    scrollportfolio.style.setProperty('--rotacion', principio.rotacion + window.scrollY * rapidez.rotacion + 'deg');
-    scrollportfolioTitulo.style.setProperty('--fontsize', principio.fontsize + window.scrollY * rapidez.fontsize + 'px');
+    scrollportfolio.style.setProperty('--rotate', principio.rotacion + window.scrollY * rapidez.rotacion + 'deg');
+    scrollportfolioTitulo.style.setProperty('--opacity', principio.opacidad - window.scrollY * rapidez.opacidad);
 
   });
 }
