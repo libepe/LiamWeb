48b6cba3493133af7a9c2d03357b432033cbd536
libepe <liam.beus@gmail.com>
2026-03-18 15:04:17 +0100

    mod nueva

--- js/general.js
+++ js/general.js
@@ -1,10 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
-    coverExplosion();
-
+    
+fallingDroplet();
 });
 
 
 
+function fallingDroplet(){
+  const Paintdrops=document.querySelectorAll('.paintdrops');
+
+  Paintdrops.forEach((paintdrop, paintdropIndex)=>{
+    const Drops=paintdrop.querySelectorAll('.drop');
+    const retraso=paintdropIndex * 500;
+
+    setTimeout(()=>{
+      Drops.forEach((drop)=>{
+
+        paintdrop.style.opacity='0.2';
+        paintdrop.style.transition='opacity 0.5s';
+
+        const randomHeight=Math.floor(Math.random()*600)+200;
+        const randomDuration = Math.random() * 3 + 1;
+        drop.style.height= randomHeight + 'px';
+        drop.style.transition=` height ${randomDuration}s`;
+      });
+
+    },retraso);
+
+    setTimeout(()=>{
+      paintdrop.style.opacity= '0';
+    },retraso + 1000)
+
+    setTimeout(()=>{
+      Drops.forEach((drop)=>{
+        drop.style.height= '0';
+      })
+      if (paintdropIndex==Paintdrops.length - 1) {
+        fallingDroplet();
+      }
+    },retraso + 1200)
+
+  });
+
+
+}
 
 
 
@@ -19,9 +57,9 @@ function slowScrollTo(topag, pag) {
     window.scrollTo({ top: arriba + 10, behavior: 'smooth' });
   });
 }
-slowScrollTo('toportfolio', 'pag-portfolio');
-slowScrollTo('toinicio', 'pag-inicio');
-slowScrollTo('tocontacto', 'pag-contacto');
+//slowScrollTo('toportfolio', 'pag-portfolio');
+//slowScrollTo('toinicio', 'pag-inicio');
+//slowScrollTo('tocontacto', 'pag-contacto');
 
 
 
